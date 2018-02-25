fe1551168942d0cfa98ac877ccf15203cec64e15
monty <amos.samuel@hotmail.com>
2018-02-24 19:09:26 -0500

    change matrice de String à une Matrice de char
    create PathFinder in Runner

--- java/src/main/java/ca/umontreal/iro/hackathon/loderunner/Runner.java
+++ java/src/main/java/ca/umontreal/iro/hackathon/loderunner/Runner.java
@@ -1,5 +1,7 @@
 package ca.umontreal.iro.hackathon.loderunner;
 
+import ca.hackermen.caroline.PathFinder;
+
 /**
  *
  */
@@ -7,14 +9,16 @@ public class Runner extends BasicRunner {
 
     // TODO : Remplacer ceci par votre clé secrète
     public static final String ROOM = "caroline18";
-
+    
     /* Utilisez cette variable pour choisir le niveau de départ
      *
      * Notez: le niveau de départ sera 1 pour tout
      * le monde pendant la compétition :v)
      */
     public static final int START_LEVEL = 1;
-
+    
+    public PathFinder pathFinder;
+    public char[][] mapMatrice;
     public Runner() {
         super(ROOM, START_LEVEL);
     }
@@ -22,12 +26,24 @@ public class Runner extends BasicRunner {
     @Override
     public void start(String[] grid) {
         System.out.println("Nouveau niveau ! Grille initiale reçue :");
+        char[][] matriceMap; 
+        String ligne = grid[0];
+        
+        
+        
+        matriceMap = new char[grid.length][ligne.length()];
 
-        for (int i=0; i<grid.length; i++) {
-            String ligne = grid[i];
-
-            System.out.println(ligne);
+        for (int i = 0; i < matriceMap.length; i++) {
+            ligne = grid[i];
+            for (int j = 0; j < matriceMap[i].length; j++) {
+                
+                matriceMap[i][j] = ligne.charAt(j);
+                
+            }
         }
+        
+        pathFinder = new PathFinder(matriceMap);
+        
     }
 
     @Override
